e593fe65efc63ed0cdf441170faa8116049a0c14
Caroline Spiezio <spieziocaroline@gmail.com>
2022-03-01 14:30:50 -0800

    six tests written and passing for anagrams

--- src/anagrams.js
+++ src/anagrams.js
@@ -3,4 +3,12 @@ import { getLetterCount } from './letter-count.js';
 export const isAnagram = (str1, str2) => {
   if (str1.length === 0 && str2.length === 0) return true;
   if (str1.length !== str2.length) return false;
+
+  const str1Count = getLetterCount(str1);
+  const str2Count = getLetterCount(str2);
+
+  for (let key in str1Count) {
+    if (str1Count[key] !== str2Count[key]) return false;
+  }
+  return true;
 };
--- src/anagrams.test.js
+++ src/anagrams.test.js
@@ -12,4 +12,24 @@ describe('isAnagram - basic functionality', () => {
     const actual = isAnagram('hi', 'bye');
     expect(actual).to.equal(expected);
   });
+  it('returns false if one word has one character that is different from the other word', () => {
+    const expected = false;
+    const actual = isAnagram('taco', 'cati');
+    expect(actual).to.equal(expected);
+  });
+  it('returns false if one word has multiple letters different from the other word', () => {
+    const expected = false;
+    const actual = isAnagram('tacorita', 'catimeca');
+    expect(actual).to.equal(expected);
+  });
+  it('returns true if both words have the same number of the same character and just one of each character', () => {
+    const expected = true;
+    const actual = isAnagram('tac', 'cat');
+    expect(actual).to.equal(expected);
+  });
+  it('returns true if both words have the same number of the same character and multiple of each character', () => {
+    const expected = true;
+    const actual = isAnagram('aracecare', 'eracecara');
+    expect(actual).to.equal(expected);
+  });
 });

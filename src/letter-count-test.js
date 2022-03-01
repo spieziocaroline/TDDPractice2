c80b0afdc22986fb8470ed1924801ba8ba161d95
Caroline Spiezio <spieziocaroline@gmail.com>
2022-03-01 14:00:18 -0800

    wrote and passed second test

--- src/letter-count-test.js
+++ src/letter-count-test.js
@@ -10,4 +10,9 @@ describe('getLetterCount - basic functionality', () => {
     const actual = getLetterCount('');
     expect(actual).to.deep.equal(expected);
   });
+  it('returns the correct object for word with only one of each letter', () => {
+    const expected = { c: 1, a: 1, t: 1 };
+    const actual = getLetterCount('cat');
+    expect(actual).to.deep.equal(expected);
+  });
 });
--- src/letter-count.js
+++ src/letter-count.js
@@ -1,3 +1,10 @@
 export const getLetterCount = (str) => {
-  return {};
+  const letterCount = {};
+  const letters = str.split('');
+  letters.forEach((letter) => {
+    if (!letterCount[letter]) {
+      letterCount[letter] = 1;
+    }
+  });
+  return letterCount;
 };
